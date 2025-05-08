44adc20e930f83195ac31b1d70ce73f45eab39a9
Lizamwol <85516053+NithinLiza@users.noreply.github.com>
2025-05-08 14:32:21 +0530

    Update owner.js

--- commands/owner.js
+++ commands/owner.js
@@ -1,17 +1,33 @@
-const settings = require('../settings');
-
-async function ownerCommand(sock, chatId) {
-    const vcard = `
-BEGIN:VCARD
-VERSION:3.0
-FN:${settings.botOwner}
-TEL;waid=${settings.+918137829228}:${settings.+918137829228}
-END:VCARD
-`;
-
-    await sock.sendMessage(chatId, {
-        contacts: { LIZAMWOL: settings.Mudiyanmass, +918137829228: [{ vcard }] },
-    });
-}
-
-module.exports = ownerCommand;
+const { izumi, mode } = require('../lib/');
+izumi({
+    pattern: "owner$",
+    fromMe: mode,
+    desc: "Bot Owner",
+    type: "user",
+}, async (message, match, client) => {
+    try {
+        const name = 'Eʏᴘᴢ ☔', title = "Iᴢᴜᴍɪ Sᴜᴘᴘᴏʀᴛ🧚‍♂️", number = '917994489493', body = "Eʏᴘᴢ☔";
+        const image = "https://i.imgur.com/UriXD0j.jpeg", sourceUrl = 'https://chat.whatsapp.com/KHvcGD7aEUo8gPocJsYXZe';
+        const vcard = `BEGIN:VCARD\nVERSION:3.0\nFN:${name}\nTEL;type=CELL;type=VOICE;waid=${number}:${number}\nEND:VCARD`;
+        await client.sendMessage(message.jid, {
+            contacts: {
+                contacts: [{ vcard }]
+            },
+            contextInfo: { 
+                externalAdReply: {
+                    title: `${title}`,
+                    body: `${body}`,
+                    sourceUrl: `${sourceUrl}`,
+                    mediaUrl: `${sourceUrl}`,
+                    mediaType: 1,
+                    showAdAttribution: true,
+                    renderLargerThumbnail: true,
+                    thumbnailUrl: `${image}`
+                }
+            }
+        });
+    } catch (error) {
+        console.error('Error occurred:', error);
+        await client.sendMessage(message.jid, { text: '```Error occurred while executing the command.```' });
+    }
+});
